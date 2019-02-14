7a1d4efc3877dfbf1f510c4bcd6ce6883de3be59
richardsforu <praveen.somireddy@gmail.com>
2019-02-14 15:22:13 +0530

    boot added

---
+++ Boot-WS/Boot-JdbcTemplate/src/main/java/com/cts/product/BootJdbcTemplateApplication.java
@@ -0,0 +1,14 @@
+package com.cts.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJdbcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJdbcTemplateApplication.class, args);
+	}
+
+}
+
